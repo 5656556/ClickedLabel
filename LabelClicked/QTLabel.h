2fd2102e3762d2393c206375d5c102e5018b7aea
秦领 <qinling@ihbaby.com>
2018-05-24 15:33:25 +0800

    first commit

---
+++ LabelClicked/QTLabel.h
@@ -0,0 +1,13 @@
+//
+//  QTLabel.h
+//  LabelClicked
+//
+//  Created by Rey on 2018/5/24.
+//  Copyright © 2018年 Rey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QTLabel : UILabel
+
+@end
---
+++ LabelClicked/QTLabel.m
@@ -0,0 +1,21 @@
+//
+//  QTLabel.m
+//  LabelClicked
+//
+//  Created by Rey on 2018/5/24.
+//  Copyright © 2018年 Rey. All rights reserved.
+//
+
+#import "QTLabel.h"
+
+@implementation QTLabel
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
